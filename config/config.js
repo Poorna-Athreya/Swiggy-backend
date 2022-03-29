f5f4f9b440d2924a21affc780b797de8e771e2bf
Aishwarya S R <Aishwarya_S_R@mckinsey.com>
2022-03-29 17:45:47 +0530

    improv: change test and production env

--- config/config.js
+++ config/config.js
@@ -11,16 +11,16 @@ module.exports = {
   },
   test: {
     username: process.env.DB_USER,
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.HOST,
     dialect: 'postgres',
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.HOST,
     dialect: 'postgres',
   },
 };
