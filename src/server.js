18c5141c2e765bb4258959eea216075926009882
Poorna-Athreya <poorna_athreya@mckinsey.com>
2022-03-29 11:33:58 +0530

    Chore: Creating database

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const env = require('dotenv');
+const bodyParser = require('body-parser');
+
+env.config();
+const port = process.env.PORT || 8000;
+const host = process.env.HOST || 'localhost';
+
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+    console.log(`Server listening at: http://${host}:${port}`);
+});
